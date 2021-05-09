6a8c459e512e1ac843d5c9d1f6a789ca50a146b6
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-05-09 04:04:39 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import DashboardCore from "components/DashboardCore";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <NavBar />
+      <DashboardCore />
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
