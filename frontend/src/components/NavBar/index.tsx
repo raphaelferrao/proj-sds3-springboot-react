73361183e3f86e53e7ceb89773644c25e9f85827
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-05-09 04:14:46 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,6 @@
 
+import { Link } from 'react-router-dom';
+
 import ImgDsDark from 'assets/img/ds-dark.svg';
 
 const NavBar = () => {
@@ -6,7 +8,9 @@ const NavBar = () => {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={ImgDsDark} alt="DevSuperior" width="120" />
+          <Link to="/" title="Home">
+            <img src={ImgDsDark} alt="DevSuperior" width="120" />
+          </Link>
         </nav>
       </div>
     </div>
